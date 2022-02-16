a61cca1784345cc255f24a9d5dc6c3613bf1491f
MajoretteKay <keolecki@gmail.com>
2022-02-16 00:03:11 -0500

    navigation bar, basic routing, and a steady theme set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    loadChildren: () => {
+      return import("./components/home/home.module")
+      .then((m) => {
+        return m.HomeModule;
+      });
+    }, path: ""
+  },
+  {
+    path: "**",
+    redirectTo: ""
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
