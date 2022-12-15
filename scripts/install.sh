0f7035e212a10761dd95abf593294e1c5e2407d5
Roshnee Sharma <37020881+roshnee99@users.noreply.github.com>
2022-12-14 22:08:06 -0500

    Create install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,9 @@
+echo "Installing Required Commands"
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+sh . ~/.nvm/nvm.sh
+echo "Activiated nvm"
+echo "Installing npm"
+nvm install 16
+echo "Installing http-server"
+npm install http-server -g
