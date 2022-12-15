faa3bc840a7f9c3e5da28bc83f09a87c9b2c6433
Roshnee Sharma <37020881+roshnee99@users.noreply.github.com>
2022-12-14 22:09:53 -0500

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,8 +2,5 @@ echo "About to start up!"
 current_dir=$(pwd)
 deployment_dir=${current_dir}/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 
-echo "Intalling http-server"
-npm install http-server -g
-
 echo "Launching website"
-http-server ${deployment_dir}/dist/arfna-website >> ${log_file} 2>&1 &
+http-server ${deployment_dir}/arfna-website >> ${log_file} 2>&1 &
