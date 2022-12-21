8f7312ba7b29f0c29a806ae77bc228f7f13a9dcf
Roshnee Sharma <37020881+roshnee99@users.noreply.github.com>
2022-12-21 18:20:50 -0500

    CU-3xwha6q Copying files from deployment group to root directory (#10)

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -9,8 +9,13 @@ echo "Installing http-server"
 npm install http-server -g
 
 current_dir=$(pwd)
-deployment_dir=${current_dir}/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
+deployment_group_deployment_dir=${current_dir}/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
+deployment_dir=/deployment
 log_file=${deployment_dir}/log.txt
 
+echo "Copying files from deployment group directory to root"
+website=${deployment_dir}/arfna-website
+mkdir ${deployment_dir} && cp -r ${deployment_group_deployment_dir}/arfna-website ${website}
+
 echo "Launching website at port 8080"
-http-server ${deployment_dir}/arfna-website >> ${log_file} 2>&1 &
+http-server ${website} >> ${log_file} 2>&1 &
