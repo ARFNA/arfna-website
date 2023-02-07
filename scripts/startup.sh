15a72f361cce47a79acea238b6b73af5919ce201
roshnee99 <rs171999@gmail.com>
2023-02-07 11:27:29 -0800

    Adjusted startup script to install express

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -16,5 +16,9 @@ echo "Copying files from deployment group directory to root"
 website=${deployment_dir}/arfna-website
 sudo rm -rf ${deployment_dir} && mkdir ${deployment_dir} && sudo cp -r ${deployment_group_deployment_dir}/arfna-website ${deployment_dir}
 
+echo "Installing express 4.18.2 as node-modules must be present"
+cd ${website}
+npm install express@4.18.2
+
 echo "Launching website at port 8080"
-http-server ${website} >> ${log_file} 2>&1 &
+node ${website}/server.js >> ${log_file} 2>&1 &
